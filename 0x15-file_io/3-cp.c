36b3a72490a431cf2c0d95cd7e4f6144d2c77f55
Code-chuks <Ezeuzorchukwudi@gmail.com>
2023-10-06 19:19:03 -0700

    soul travail

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -85,7 +85,6 @@ int main(int argc, char *argv[])
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-																			
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
